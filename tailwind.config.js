3ae4c87f38ce16061614d648f56dc43c1da91c24
Casen Luck <38961600+cluck135@users.noreply.github.com>
2022-07-28 17:04:42 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+      'Charcoal': '#38383B',
+      'Gallery': '#EBEBEB',
+      'Accent': '#FEB016',
+    },
+  },
+    
+  },
+  plugins: [],
+}
